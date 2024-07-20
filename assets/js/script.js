e49d58a35d2df298735ebc717deac8828aa09abb
richi2up <richard@5883>
2024-07-19 22:37:08 -0400

    ultimos cambios

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//js para popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+
+
+
+
+$(document).ready(function() {
+    $("#enviarCorreo").click(function() {
+        alert("El correo fue enviado correctamente...");
+    })
+});
